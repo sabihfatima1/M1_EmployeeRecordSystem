dd597939749a95cce3d010fedfb9b7c0056543c2
sabihfatima1 <101128461+sabihfatima1@users.noreply.github.com>
2022-04-03 15:19:36 +0530

    Add files via upload

---
+++ 3_Implementation/documentation/employee_data.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<windows.h>
+
+struct emp
+    {
+        char name[40];
+        int age;
+        int e_id;
+        float salary;
+    };
+
+struct emp e;
+    
